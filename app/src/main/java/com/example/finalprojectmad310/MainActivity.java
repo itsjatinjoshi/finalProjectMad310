72653398b0677e260ecf802b5fef5ebebddf1f1b
dixit gandhi <gandhi.dixit5@gmail.com>
2019-07-13 19:59:35 -0400

    changes by sharad

--- app/src/main/java/com/example/finalprojectmad310/MainActivity.java
+++ app/src/main/java/com/example/finalprojectmad310/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.finalprojectmad310;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
