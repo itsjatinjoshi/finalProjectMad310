92b8c698e6ec528c94721c5e4d265685ae969375
itsjatinjoshi <jatinjoshi2283@gmail.com>
2019-07-15 01:53:13 -0400

    changes

--- app/src/main/java/com/example/finalprojectmad310/Mylistadapter.java
+++ app/src/main/java/com/example/finalprojectmad310/Mylistadapter.java
@@ -15,64 +15,64 @@ import java.util.ArrayList;
 
 public class Mylistadapter extends BaseAdapter {
 
-    Context c;
-    ArrayList<Products> pro;
 
-    LayoutInflater inflater;
+        Context c;
+        ArrayList<Products> pro;
 
-    private View.OnClickListener itemlisten;
+        LayoutInflater inflater;
+        private View.OnClickListener itemlisten;
 
-    public Mylistadapter(Context c, ArrayList<Products> pro) {
-        this.c = c;
-        this.pro = pro;
-    }
+        public Mylistadapter(Context c, ArrayList<Products> pro) {
+            this.c = c;
+            this.pro = pro;
+        }
 
-    public Mylistadapter() {
-    }
+        public Mylistadapter() {
+        }
 
-    @Override
-    public int getCount() {
-        return pro.size();
-    }
+        @Override
+        public int getCount() {
+            return pro.size();
+        }
 
-    @Override
-    public Object getItem(int position) {
-        return pro.get(position);
-    }
+        @Override
+        public Object getItem(int position) {
+            return pro.get(position);
+        }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-
-    public void setOnItemClickListener(View.OnClickListener itemClickListener) {
-        itemlisten = itemClickListener;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        if (inflater == null)
-        {
-            inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @Override
+        public long getItemId(int position) {
+            return position;
         }
 
 
-        if(convertView == null)
-        {
-            convertView = inflater.inflate(R.layout.list_item,parent,false);
+        public void setOnItemClickListener(View.OnClickListener itemClickListener) {
+            itemlisten = itemClickListener;
         }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
 
-        ImageView pimg = convertView.findViewById(R.id.img_products);
-        TextView  pname = convertView.findViewById(R.id.txt_pname);
+            if (inflater == null)
+            {
+                inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
 
 
-        Picasso.get().load(pro.get(position).getPimg()).into(pimg);
-        pname.setText(pro.get(position).getPname());
+            if(convertView == null)
+            {
+                convertView = inflater.inflate(R.layout.list_item,parent,false);
+            }
 
-        return convertView;
+
+            ImageView pimg = convertView.findViewById(R.id.img_products);
+            TextView  pname = convertView.findViewById(R.id.txt_pname);
+
+            Picasso.get().load(pro.get(position).getPimg()).into(pimg);
+
+            pname.setText(pro.get(position).getPname());
+
+            return convertView;
     }
 
 
